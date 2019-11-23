12bcc4e7a527c7221629c1c26517f5997b0b7a2a
Youssef El Saadany <youssef.el-saadany@unchartech.com>
2019-11-23 19:16:06 +0100

    refacto and restructuration

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 import pygame as pg
 import math
+from leapy import Leapy
+import levels as lvls
 def sigmoid(x):
     return 1 / (1 + math.exp(-x))
 
@@ -19,36 +21,9 @@ y = 550
 width = 50
 height = 50
 
-class Leapy():
-    def __init__(self, x, y, width, height, color):
-        self.x = x
-        self.y = y
-        self.width = width
-        self.height = height
-        self.color = color
-
-    def jump():
-        pass
-        
-
-    def getRect(self):
-        return (self.x, self.y, self.width, self.height)
-    
-    def getColor(self):
-        return self.color
-
-    def moveLeft(self):
-        self.x -= 10
-    
-    def draw(self):
-        pg.draw.rect(window, self.getColor(), self.getRect())
-
-
-
 leapy = Leapy(x, y , width, height, (255, 0, 255))
-obstacles = []
-for i in range(4):
-    obstacles.append(Leapy(400 + (i * 100), 550, 50, 50, (255, 0, 0)))
+levels = lvls.Levels()
+
 run = True
 jumping = False
 inversing = False
@@ -58,71 +33,77 @@ transUp = False
 pg.key.set_repeat(500)
 jump_count = 10
 while run:
-    hold_return = False
-    pg.time.delay(50)
-    for event in pg.event.get():
-        if event.type == pg.QUIT:
+    level = levels.getLevel1()
+    inGame = True
+    while inGame:
+        hold_return = False
+        pg.time.delay(50)
+        for event in pg.event.get():
+            if event.type == pg.QUIT:
+                inGame = False
+                run = False
+
+        keys = pg.key.get_pressed()
+        if (keys[pg.K_RETURN] and not jumping and not inverseJump
+            and not transDown and not transUp):
+            if inversing:
+                inversing = False
+                transDown = True
+            else:
+                inversing = True
+                transUp = True
+        if keys[pg.K_SPACE] and not inversing and not transDown and not transUp:
+            jumping = True
+        if keys[pg.K_SPACE] and inversing and not transDown and not transUp:
+            inverseJump = True
+        if keys[pg.K_q]:
+            inGame = False
             run = False
 
-    keys = pg.key.get_pressed()
-    if (keys[pg.K_RETURN] and not jumping and not inverseJump
-        and not transDown and not transUp):
+        if jumping:
+            if jump_count >= -10:
+                neg = 1
+                if jump_count < 0:
+                    neg = -1
+                leapy.y -= (jump_count ** 2) * 0.5 * neg
+                jump_count -= 1
+            else:
+                jumping = False
+                jump_count = 10
+
+        if inverseJump:
+            if jump_count >= -10:
+                neg = 1
+                if jump_count < 0:
+                    neg = -1
+                leapy.y += (jump_count ** 2) * 0.5 * neg
+                jump_count -= 1
+            else:
+                inverseJump = False
+                jump_count = 10
+
         if inversing:
-            inversing = False
-            transDown = True
-        else:
-            inversing = True
-            transUp = True
-    if keys[pg.K_SPACE] and not inversing and not transDown and not transUp:
-        jumping = True
-    if keys[pg.K_SPACE] and inversing and not transDown and not transUp:
-        inverseJump = True
-    if keys[pg.K_q]:
-        run = False
-
-    if jumping:
-        if jump_count >= -10:
-            neg = 1
-            if jump_count < 0:
-                neg = -1
-            leapy.y -= (jump_count ** 2) * 0.5 * neg
-            jump_count -= 1
-        else:
             jumping = False
-            jump_count = 10
 
-    if inverseJump:
-        if jump_count >= -10:
-            neg = 1
-            if jump_count < 0:
-                neg = -1
-            leapy.y += (jump_count ** 2) * 0.5 * neg
-            jump_count -= 1
-        else:
-            inverseJump = False
-            jump_count = 10
+        if transDown:
+            if leapy.y < 550:
+                leapy.y += 50
+            else:
+                transDown = False
 
-    if inversing:
-        jumping = False
-
-    if transDown:
-        if leapy.y < 550:
-            leapy.y += 50
-        else:
-            transDown = False
-
-    if transUp:
-        if leapy.y > 0:
-            leapy.y -= 50
-        else:
-            transUp = False
-        
-    window.fill((255, 255, 255))
-    if True in [collide(leapy, obs) for obs in obstacles]:
-        run = False
-    leapy.draw()
-    [obs.draw() for obs in obstacles]
-    [obs.moveLeft() for obs in obstacles]
-    pg.display.update()
+        if transUp:
+            if leapy.y > 0:
+                leapy.y -= 50
+            else:
+                transUp = False
+            
+        window.fill((255, 255, 255))
+        if True in [collide(leapy, obs) for obs in level.obstacles]:
+            inGame = False
+            run = False
+        leapy.draw(window)
+        [obs.draw(window) for obs in level.obstacles]
+        [obs.moveLeft() for obs in level.obstacles]
+        pg.display.update()
 
 pg.quit()
