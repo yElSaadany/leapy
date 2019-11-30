83fa5e3c0736728af4b134b08759416c2e6669a6
Youssef El Saadany <youssef.saadany@gmail.com>
2019-11-30 16:22:09 +0100

    modified level1, levels class and main to work with coins

--- levels.py
+++ levels.py
@@ -1,17 +1,15 @@
-from leapy import Leapy
+from leapy import Leapy, Coin
 
 
 class Level():
-    def __init__(self, obstacles):
+    def __init__(self, obstacles, coins):
         self.obstacles = obstacles
+        self.coins = coins
 
 
 class Levels():
     def __init__(self):
         self.levels = []
-        self.levels.append(Level([Leapy(400 + (i * 100), 550, 50, 50,
-                                        (255, 0, 0))
-                                  for i in range(4)]))
 
     def addLevel(self, level):
         self.levels.append(level)
@@ -24,11 +22,14 @@ class Levels():
         with open(levelFile, 'r') as level:
             lines = level.readlines()
         
-        level = Level([])
+        level = Level([], [])
         for line in lines:
             tmp = line.split(',')
             tmp[-1] = tmp[-1][:-1]
             tmp = list(map(int, tmp))
-            level.obstacles.append(Leapy(tmp[1], tmp[2], 50, 50,
-                                   (255, 0, 0)))
+            if tmp[0] == 0:
+                level.obstacles.append(Leapy(tmp[1], tmp[2], 50, 50,
+                                       (255, 0, 0)))
+            elif tmp[0] == 1:
+                level.coins.append(Coin(tmp[1], tmp[2]))
         return self.addLevel(level)
--- main.py
+++ main.py
@@ -1,46 +1,26 @@
 import pygame as pg
-from leapy import Leapy
+from leapy import Leapy, Coin
 import levels as lvls
+from physics import collide
 
-
-def collide2(element1, element2):
-    if (element2.x <= (element1.x + element1.width) and
-        element2.y <= (element1.y + element1.height)):
-        return True
-
-def collide3(element1, element2):
-    hb_x = element1.getHitbox('x')
-    hb_y = element1.getHitbox('y')
-    if (element2.x >= hb_x[0] and element2.x <= hb_x[1] and
-        element2.y >= hb_y[0] and element2.y <= hb_y[1]):
-        return True
-
-def pointInHitbox(point, element):
-    if (point[0] >= element.x and
-        point[0] <= (element.x + element.width) and
-        point[1] >= element.y and
-        point[1] <= (element.y + element.height)):
-        return True
-    
-def collide(element1, element2):
-    if True in [pointInHitbox(point, element2)
-                for point in element1.getPoints()]:
-        return True
 
 pg.init()
 
 window = pg.display.set_mode((600, 600))
 pg.display.set_caption("Leapy")
+bg_image = pg.image.load('assets/bg.png').convert()
 
 x = 100
 y = 550
 width = 50
 height = 50
 
-leapy = Leapy(x, y , width, height, (255, 0, 255))
+leapy = Leapy(x, y, width, height, (255, 0, 255))
+coin = Coin(250, 250)
 levels = lvls.Levels()
 print(levels.importLevel('data/levels/level1.lvl'))
 
+score = 0
 run = True
 jumping = False
 inversing = False
@@ -51,7 +31,7 @@ pg.key.set_repeat(500)
 jump_count = 10
 while run:
     # todo: add end game
-    level = levels.levels[1]
+    level = levels.levels[0]
     inGame = True
     while inGame:
         hold_return = False
@@ -63,14 +43,15 @@ while run:
 
         keys = pg.key.get_pressed()
         if (keys[pg.K_RETURN] and not jumping and not inverseJump
-            and not transDown and not transUp):
+                and not transDown and not transUp):
             if inversing:
                 inversing = False
                 transDown = True
             else:
                 inversing = True
                 transUp = True
-        if keys[pg.K_SPACE] and not inversing and not transDown and not transUp:
+        if (keys[pg.K_SPACE] and not inversing and not transDown
+                and not transUp):
             jumping = True
         if keys[pg.K_SPACE] and inversing and not transDown and not transUp:
             inverseJump = True
@@ -114,14 +95,23 @@ while run:
                 leapy.y -= 50
             else:
                 transUp = False
-            
+
         window.fill((255, 255, 255))
         if True in [collide(leapy, obs) for obs in level.obstacles]:
             inGame = False
             run = False
-        leapy.draw(window)
+
+        coins_collisions = [collide(leapy, coin) for coin in level.coins]
+        if True in coins_collisions:
+            del level.coins[coins_collisions.index(True)]
+            score += 1
+            print(score)
+        window.blit(bg_image, [0, 0])
+        [coin.draw(window) for coin in level.coins]
+        [coin.moveLeft() for coin in level.coins]
         [obs.draw(window) for obs in level.obstacles]
         [obs.moveLeft() for obs in level.obstacles]
+        leapy.draw(window)
         pg.display.update()
 
 pg.quit()
