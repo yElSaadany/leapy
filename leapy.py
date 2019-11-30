ee8b0d5edd2cafd6a4fdd5362565e43ceb183212
Youssef El Saadany <youssef.saadany@gmail.com>
2019-11-30 16:20:03 +0100

    added coin class

--- leapy.py
+++ leapy.py
@@ -11,16 +11,16 @@ class Leapy():
 
     def jump():
         pass
-        
+
     def getRect(self):
         return (self.x, self.y, self.width, self.height)
-    
+
     def getColor(self):
         return self.color
 
     def moveLeft(self):
         self.x -= 10
-    
+
     def draw(self, window):
         pg.draw.rect(window, self.getColor(), self.getRect())
 
@@ -35,3 +35,19 @@ class Leapy():
                 (self.x + self.width, self.y),
                 (self.x, self.y + self.height),
                 (self.x + self.width, self.y + self.height)]
+
+
+class Coin():
+    image = pg.image.load('assets/coin.png')
+
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+        self.width = self.image.get_width()
+        self.height = self.image.get_height()
+
+    def moveLeft(self):
+        self.x -= 10
+
+    def draw(self, window):
+        window.blit(self.image, (self.x, self.y))
